46b46751fbb6419fca95570dc6b3be6d87c5c174
wildthinkslaboratory <goldfishandrobin@gmail.com>
2026-04-28 14:20:05 -0700

    moving more files into subdirectories

---
+++ run_experiments.py
@@ -0,0 +1,13 @@
+# uncomment the experiments you want to run
+
+# experiments for tuning orthogonal collocation encoding of NLP
+# from experiments import oc_experiments
+
+# experiments for tuning multiple shooting encoding of NLP
+from experiments import ms_experiments
+
+# experiments for tuning Chebyshev pseudo-spectral collocation encoding of NLP
+# from experiments import cps_experiments
+
+# experiments that compare the three NLP encodings
+# from experiments import nlp_experiment
