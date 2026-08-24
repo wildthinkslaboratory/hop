c2cc48b5cb786979031ed2fd90c6fa1a26954737
wildthinkslaboratory <goldfishandrobin@gmail.com>
2026-08-24 10:49:29 -0700

    add plotting to flight data class

--- flight_analysis_tools/flight_data.py
+++ flight_analysis_tools/flight_data.py
@@ -1,7 +1,10 @@
 from hop.utilities import import_data
 import sys
 import numpy as np
+from plotting.plots import plot_state, plot_control, plot_pwm, plot_attitude, plot_parameters
+from hop.utilities import quaternion_to_angle
 
+import matplotlib.pyplot as plt
 
 class FlightData:
     def __init__(self, filename='./plotter_logs/current.json'):
@@ -26,6 +29,7 @@ class FlightData:
         self.pwm_motors = np.empty([len(data),2])
         self.pwm_servos = np.empty([len(data),2])
         self.parameters = np.empty([len(data),5])
+        self.attitude = np.empty([len(data),3])
         self.timing_data = []
         self.voltage = []
         self.timestamps = []
@@ -44,6 +48,11 @@ class FlightData:
                 self.parameters[i] = np.array(d['parameters'])
             self.voltage.append(d['parameters'][3])
 
+            # turn quaternions into attitude
+            q = np.reshape(self.state_data[i][6:10].copy(), (4,))
+            self.attitude[i] = quaternion_to_angle(q)
+
+
         stop_index = int(self.start_time // self.dt)
         self.len_used_data = len(data) - stop_index -1
 
@@ -54,5 +63,23 @@ class FlightData:
         self.timestamps = self.timestamps[stop_index+1:]
         self.pwm_motors = self.pwm_motors[stop_index+1:]
         self.pwm_servos = self.pwm_servos[stop_index+1:]
-        self.parameters = self.parameters[stop_index+1:-1]
+        self.parameters = self.parameters[stop_index+1:]
         self.timing_data = self.timing_data[stop_index+1:]
+
+
+    def plot(self, begin=0, end=-1):
+
+        if end == -1:
+            end = self.len_used_data
+
+        # verify our ranges
+        assert(begin >= 0 and begin < self.len_used_data)
+        assert(end >= 0 and end <= self.len_used_data)
+
+        tspan = np.arange(0, (end-begin) * self.dt , self.dt)
+        plot_state(tspan, self.state_data[begin:end], 'flight data state')
+        plot_control(tspan, self.control_data[begin:end], 'flight data control')
+        plot_parameters(tspan, self.parameters[begin:end], 'flight parameters')
+        plot_pwm(tspan, self.pwm_servos[begin:end], self.pwm_motors[begin:end], 'flight data pwm')
+        plot_attitude(tspan, self.attitude[begin:end], 'flight data attitude')
+        plt.show()
